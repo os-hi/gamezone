d56ee10bd57cccce395b8735c12afd1f082033a2
Jake <110341554+os-hi@users.noreply.github.com>
2023-12-28 01:34:22 +0800

    feat: added aboutdetails screen

---
+++ screens/AboutDetails.js
@@ -0,0 +1,10 @@
+import { StyleSheet, View, Text } from "react-native";
+const AboutDetails = () => {
+    return ( 
+        <View>
+            <Text>AboutDetails</Text>
+        </View>
+     );
+}
+ 
+export default AboutDetails;
