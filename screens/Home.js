345de04657935244b19a459894c6a33c7edc741b
Jake <110341554+os-hi@users.noreply.github.com>
2023-12-28 01:34:31 +0800

    feat: added home screen

---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import { StyleSheet, View, Text } from "react-native";
+import {globalStyles} from '../styles/global'
+const Home = () => {
+    return ( 
+        <View style={globalStyles.container}>
+            <Text>Home</Text>
+        </View>
+     );
+}
+
+export default Home;
